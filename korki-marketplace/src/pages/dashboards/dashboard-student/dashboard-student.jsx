02c26e51b57c8ab4f89a37f3bb69eb7c7dab34ba
mblkx <bilykmaciej@gmail.com>
2026-06-08 15:07:43 +0200

    Update dashboard-student.jsx

--- korki-marketplace/src/pages/dashboards/dashboard-student/dashboard-student.jsx
+++ korki-marketplace/src/pages/dashboards/dashboard-student/dashboard-student.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../../components/navbar";
-import Footer from "../../components/footer";
+import Navbar from "../../../components/navbar";
+import Footer from "../../../components/footer";
 import "./dashboard-student.css";
 
 const lessons = [
